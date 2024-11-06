ba53d2ce8253f067449f459510e2bd0b592cc3c5
Unnat Malik <160745526+UnnatMalik@users.noreply.github.com>
2024-11-06 10:15:50 +0530

    Update settings.py

--- DBMSwebsite/settings.py
+++ DBMSwebsite/settings.py
@@ -4,9 +4,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'ENTER YOUR SECRET KEY'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
